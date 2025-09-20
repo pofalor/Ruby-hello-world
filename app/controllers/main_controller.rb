789b981d268f7d451c011f8adb5b2e41074d0c1b
Даниил <gromovdk@stud.kai.ru>
2025-09-20 09:30:03 +0300

    revert changes date and routes

--- app/controllers/main_controller.rb
+++
@@ -1,8 +0,0 @@
-class MainController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
-
-  def helloDate
-    render html: "¡Hola, mundo! " + Date.today.to_s
-  end
-end
